68b23a2049fa9ede08991467951d574e964f70ae
Airshipster <160562833+Airshipster@users.noreply.github.com>
2026-04-10 21:53:14 +0400

    Update configuration settings in config.py

--- src/config.py
+++ src/config.py
@@ -8,10 +8,14 @@ SHEET_NAME_PUSH_EVENTS = "Push события"
 
 SERVICE_ACCOUNT_JSON = os.environ.get("GOOGLE_SERVICE_ACCOUNT_JSON")
 
+CALLBACK_URL = "https://script.google.com/macros/s/AKfycbwqySnqlEYAMTTQNkcUy9RU-B6UkikW9o-v5lzLxtthnpOE_52XRZThoe2b1xjIj1Zm/exec"
+
 YOUTUBE_API_KEY = None
 
 FILTER_SHORTS = True
 FILTER_LIVE = True
-MAX_VIDEO_AGE_HOURS = 168
+MAX_VIDEO_AGE_HOURS = 24
 
 TELEGRAM_RATE_LIMIT = 20
+
+DEFAULT_MESSAGE_TEMPLATE = '🎥 <b>{video_title}</b>\n\n📺 {channel_title}\n🔗 {video_url}'
