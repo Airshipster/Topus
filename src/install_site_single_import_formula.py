8bbc34dffaa2e91ccf060e44742ca38244e40d55
Airshipster <elman.ahmadbayov@gmail.com>
2026-05-25 03:24:31 +0400

    Fix single import site formula

--- src/install_site_single_import_formula.py
+++ src/install_site_single_import_formula.py
@@ -5,29 +5,29 @@ from sheets import authenticate_google_sheets
 SOURCE_SPREADSHEET_ID = "1m67OLnwzOLCjnLCj_xZG_eT6R90yXwLWLXOxkTrjDuY"
 
 FORMULA = f'''=LET(
-  ID;"{SOURCE_SPREADSHEET_ID}";
-  данные;IMPORTRANGE(ID;"Список. YouTube!A1:W2100");
-  заголовки;INDEX(данные;1;);
-  количество;ROWS(данные);
-  столбецНазваний;INDEX(данные;;MATCH("Название";заголовки;0));
-  начало;MATCH("Название";столбецНазваний;0)+1;
-  конец;MATCH("🔵";столбецНазваний;0)-1;
-  маска;(SEQUENCE(количество)>=начало)*(SEQUENCE(количество)<=конец);
-  названия;FILTER(столбецНазваний;маска);
-  ссылкиСырые;FILTER(INDEX(данные;;MATCH("/channel/";заголовки;0));маска);
-  партнерыСырые;FILTER(INDEX(данные;;MATCH("3▼ Партнёр";заголовки;0));маска);
-  видео;FILTER(INDEX(данные;;MATCH("Видео";заголовки;0));маска);
-  последниеСырые;FILTER(INDEX(данные;;MATCH("Посл. вид.";заголовки;0));маска);
-  созданСырые;FILTER(INDEX(данные;;MATCH("Создан";заголовки;0));маска);
-  тг;FILTER(INDEX(данные;;MATCH("TG-каналы партнёров";заголовки;0));маска);
-  ссылки;ARRAYFORMULA(IF(REGEXMATCH(ссылкиСырые;"https://www\\.");REGEXREPLACE(ссылкиСырые;"https://www\\.";"");ссылкиСырые));
-  партнеры;ARRAYFORMULA(IF(IFERROR(FIND("🐙";партнерыСырые;1)>0;FALSE);"🐙";""));
-  последние;ARRAYFORMULA(IF(ISNUMBER(последниеСырые);YEAR(последниеСырые);последниеСырые));
-  создан;ARRAYFORMULA(IF(ISNUMBER(созданСырые);YEAR(созданСырые);созданСырые));
-  VSTACK(
-    {{"Название"&CHAR(10)&"проекта"\\"Ссылка "&CHAR(10)&"на канал"\\"Партнёр "&CHAR(10)&"SciTopus"\\"Кол."&CHAR(10)&" видео"\\"Год послед."&CHAR(10)&" видео"\\"Год создания"&CHAR(10)&" канала"\\"TG-каналы"&CHAR(10)&" партнёров"}};
-    HSTACK(названия;ссылки;партнеры;видео;последние;создан;тг)
-  )
+  sourceId;"{SOURCE_SPREADSHEET_ID}";
+  data;IMPORTRANGE(sourceId;"Список. YouTube!A1:W2100");
+  headers;INDEX(data;1;);
+  rowCount;ROWS(data);
+  nameCol;INDEX(data;;MATCH("Название";headers;0));
+  startRow;MATCH("Название";nameCol;0)+1;
+  endRow;MATCH("🔵";nameCol;0)-1;
+  mask;(SEQUENCE(rowCount)>=startRow)*(SEQUENCE(rowCount)<=endRow);
+  names;FILTER(nameCol;mask);
+  rawLinks;FILTER(INDEX(data;;MATCH("/channel/";headers;0));mask);
+  rawPartners;FILTER(INDEX(data;;MATCH("3▼ Партнёр";headers;0));mask);
+  videos;FILTER(INDEX(data;;MATCH("Видео";headers;0));mask);
+  rawLast;FILTER(INDEX(data;;MATCH("Посл. вид.";headers;0));mask);
+  rawCreated;FILTER(INDEX(data;;MATCH("Создан";headers;0));mask);
+  tg;FILTER(INDEX(data;;MATCH("TG-каналы партнёров";headers;0));mask);
+  links;ARRAYFORMULA(IF(REGEXMATCH(rawLinks;"https://www\\.");REGEXREPLACE(rawLinks;"https://www\\.";"");rawLinks));
+  partners;ARRAYFORMULA(IF(IFERROR(FIND("🐙";rawPartners;1)>0;FALSE);"🐙";""));
+  lastYears;ARRAYFORMULA(IF(ISNUMBER(rawLast);YEAR(rawLast);rawLast));
+  createdYears;ARRAYFORMULA(IF(ISNUMBER(rawCreated);YEAR(rawCreated);rawCreated));
+  {{
+    "Название"&CHAR(10)&"проекта"\\"Ссылка "&CHAR(10)&"на канал"\\"Партнёр "&CHAR(10)&"SciTopus"\\"Кол."&CHAR(10)&" видео"\\"Год послед."&CHAR(10)&" видео"\\"Год создания"&CHAR(10)&" канала"\\"TG-каналы"&CHAR(10)&" партнёров";
+    names\\links\\partners\\videos\\lastYears\\createdYears\\tg
+  }}
 )'''
 
 
