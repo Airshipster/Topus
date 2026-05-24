f17e222f6f134c85da5c1fac8d45e5fd1732fb62
Airshipster <elman.ahmadbayov@gmail.com>
2026-05-25 03:29:04 +0400

    Restore site update timestamp in compact formula

--- src/install_site_single_import_formula.py
+++ src/install_site_single_import_formula.py
@@ -3,10 +3,13 @@ from sheets import authenticate_google_sheets
 
 
 SOURCE_SPREADSHEET_ID = "1m67OLnwzOLCjnLCj_xZG_eT6R90yXwLWLXOxkTrjDuY"
+BLANK_ROW = "\\".join(['""'] * 7)
+UPDATED_ROW = "\\".join(["updated"] + ['""'] * 6)
 
 FORMULA = f'''=LET(
   sourceId;"{SOURCE_SPREADSHEET_ID}";
   data;IMPORTRANGE(sourceId;"Список. YouTube!A1:W2100");
+  updated;IMPORTRANGE(sourceId;"'Стат. Каналы'!A2");
   headers;INDEX(data;1;);
   rowCount;ROWS(data);
   nameCol;INDEX(data;;MATCH("Название";headers;0));
@@ -26,7 +29,11 @@ FORMULA = f'''=LET(
   createdYears;ARRAYFORMULA(IF(ISNUMBER(rawCreated);YEAR(rawCreated);rawCreated));
   {{
     "Название"&CHAR(10)&"проекта"\\"Ссылка "&CHAR(10)&"на канал"\\"Партнёр "&CHAR(10)&"SciTopus"\\"Кол."&CHAR(10)&" видео"\\"Год послед."&CHAR(10)&" видео"\\"Год создания"&CHAR(10)&" канала"\\"TG-каналы"&CHAR(10)&" партнёров";
-    names\\links\\partners\\videos\\lastYears\\createdYears\\tg
+    names\\links\\partners\\videos\\lastYears\\createdYears\\tg;
+    {BLANK_ROW};
+    {BLANK_ROW};
+    {BLANK_ROW};
+    {UPDATED_ROW}
   }}
 )'''
 
@@ -37,7 +44,7 @@ def main():
     worksheet = spreadsheet.worksheet("Сайт")
     worksheet.batch_clear(["A1:G1000"])
     worksheet.update("A1", [[FORMULA]], value_input_option="USER_ENTERED")
-    print("Installed single IMPORTRANGE formula in Сайт!A1 and cleared A1:G1000")
+    print("Installed compact live import formula in Сайт!A1 and cleared A1:G1000")
 
 
 if __name__ == "__main__":
