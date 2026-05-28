56a1c92718d1f6e21255856715cd931adafa04b5
Airshipster <elman.ahmadbayov@gmail.com>
2026-05-29 00:55:25 +0400

    feat: export last video date for site filters

--- src/install_site_single_import_formula.py
+++ src/install_site_single_import_formula.py
@@ -3,8 +3,8 @@ from sheets import authenticate_google_sheets
 
 
 SOURCE_SPREADSHEET_ID = "1m67OLnwzOLCjnLCj_xZG_eT6R90yXwLWLXOxkTrjDuY"
-BLANK_ROW = "\\".join(['""'] * 7)
-UPDATED_ROW = "\\".join(["updated"] + ['""'] * 6)
+BLANK_ROW = "\\".join(['""'] * 8)
+UPDATED_ROW = "\\".join(["updated"] + ['""'] * 7)
 
 FORMULA = f'''=LET(
   sourceId;"{SOURCE_SPREADSHEET_ID}";
@@ -26,11 +26,12 @@ FORMULA = f'''=LET(
   links;ARRAYFORMULA(IF(REGEXMATCH(rawLinks;"https://www\\.");REGEXREPLACE(rawLinks;"https://www\\.";"");rawLinks));
   partners;ARRAYFORMULA(IF(IFERROR(FIND("🐙";rawPartners;1)>0;FALSE);"🐙";""));
   lastYears;ARRAYFORMULA(IF(ISNUMBER(rawLast);YEAR(rawLast);rawLast));
+  lastDates;ARRAYFORMULA(IF(ISNUMBER(rawLast);TEXT(rawLast;"yyyy-mm-dd");rawLast));
   createdYears;ARRAYFORMULA(IF(ISNUMBER(rawCreated);YEAR(rawCreated);rawCreated));
   cleanTg;ARRAYFORMULA(IF(REGEXMATCH(TO_TEXT(tg);"^\\s*-");"";tg));
   {{
-    "Название"&CHAR(10)&"проекта"\\"Ссылка "&CHAR(10)&"на канал"\\"Партнёр "&CHAR(10)&"SciTopus"\\"Кол."&CHAR(10)&" видео"\\"Год послед."&CHAR(10)&" видео"\\"Год создания"&CHAR(10)&" канала"\\"TG-каналы"&CHAR(10)&" партнёров";
-    names\\links\\partners\\videos\\lastYears\\createdYears\\cleanTg;
+    "Название"&CHAR(10)&"проекта"\\"Ссылка "&CHAR(10)&"на канал"\\"Партнёр "&CHAR(10)&"SciTopus"\\"Кол."&CHAR(10)&" видео"\\"Год послед."&CHAR(10)&" видео"\\"Год создания"&CHAR(10)&" канала"\\"TG-каналы"&CHAR(10)&" партнёров"\\"Дата послед."&CHAR(10)&" видео";
+    names\\links\\partners\\videos\\lastYears\\createdYears\\cleanTg\\lastDates;
     {BLANK_ROW};
     {BLANK_ROW};
     {BLANK_ROW};
@@ -43,7 +44,7 @@ def main():
     client = authenticate_google_sheets()
     spreadsheet = client.open_by_key(config.SPREADSHEET_ID)
     worksheet = spreadsheet.worksheet("Сайт")
-    worksheet.batch_clear(["A1:G1000", "A1000"])
+    worksheet.batch_clear(["A1:H1000", "A1000"])
     worksheet.update("A1", [[FORMULA]], value_input_option="USER_ENTERED")
     print("Installed single live Сайт formula with dynamic blue-marker boundary")
 
